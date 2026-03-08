d109424da75d0a217b46d1f8cdc39acd6c117273
Muriyad <akiramuriyad@gmail.com>
2026-03-08 15:15:06 -0400

    testing

--- AIconversation.js
+++ AIconversation.js
@@ -2,15 +2,15 @@ import { GoogleGenAI, ThinkingLevel } from "@google/genai";
 import 'dotenv/config';
 
 
-//The client gets the API key from the environment variable `GEMINI_API_KEY`.
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
 async function main() {
     const response = await ai.models.generateContentStream({
         model: "gemini-3-flash-preview",
-        contents: "hey did you see that the new AI model just dropped?",
+        contents: "INSERT MESSAGE CONTENTS HERE",
         config: {
-            systemInstruction: "You are a dissapointed friend, talking to another friend who is obsessed with AI. You want to convince them to stop using AI and go outside more.",
+            systemInstruction: "INSERT PERSONALITY INSTRUCTION HERE",
             thinkingConfig: {
                 thinkingLevel: ThinkingLevel.LOW,
             }
